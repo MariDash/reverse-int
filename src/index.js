f2f89202512b9a6c00cbfc3142db6b7e86656401
Maria <sheyko.mari@mail.ru>
2021-03-12 10:49:21 +0300

    feat: new decision

--- src/index.js
+++ src/index.js
@@ -1,4 +1,9 @@
 module.exports = function reverse(n) {
-    let str = '' + n;
-    return (str.split("").reverse().join(""));
+    if (n >= 0) {
+        let str = '' + n;
+        return (str.split("").reverse().join(""));
+    } else {
+        let str = '' + (n * (-1));
+        return (str.split("").reverse().join(""));
+    }
 }
